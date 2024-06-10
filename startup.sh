8700b9671a3aa16e9ed2b3db238c270e9acab8f6
Anshul Mishra <anshilmishra8032@gmail.com>
2024-06-10 23:57:20 +0530

    Initial commit

---
+++ startup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Update package lists
+apt-get update
+
+# Install the necessary dependencies
+apt-get install -y libjpeg-dev
+
+# Run the gunicorn server
+exec gunicorn --bind 0.0.0.0:8000 app:app
